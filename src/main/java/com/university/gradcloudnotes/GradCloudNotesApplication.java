2ab2bf0dcc4ec95868b0e67db74bf70ae38f723a
xiaojian <1769005891@qq.com>
2022-01-14 12:00:10 +0800

    空工程

---
+++ src/main/java/com/university/gradcloudnotes/GradCloudNotesApplication.java
@@ -0,0 +1,13 @@
+package com.university.gradcloudnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradCloudNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradCloudNotesApplication.class, args);
+    }
+
+}
